d1c57117cd77181bd191af1bd0eb30c7d0a89871
zitro <bryan@bryanortiz.com>
2018-02-02 14:20:09 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+
+class Anagram
+
+attr_accessor :word, :match
+
+	def initialize(word)
+		@word = word
+	end
+
+	def match(array)
+		#arr_split = array.split(' ')
+		#array.select{|words| words.match == @word}
+		 array.select {|words| words.split("").sort == @word.split("").sort}
+	end
+
+
+end
