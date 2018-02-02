e669e4732b7f7dc310b77a19eb75f7e4bf2dc068
harenarium <22226614+harenarium@users.noreply.github.com>
2018-02-02 13:58:35 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    self.word = word
+  end
+
+  def match(array)
+    array.select{ |word_in_array|
+      word_in_array.split("").sort == word.split("").sort
+    }
+  end
+
+
+end
