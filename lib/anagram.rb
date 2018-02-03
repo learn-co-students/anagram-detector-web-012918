ee12776477dd82ca398f3f2eb03caad25fdde607
porzingod <jasonlim.ny@gmail.com>
2018-02-03 18:14:58 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :sorted_word
+
+  def initialize(string)
+    @sorted_word = string.split("").sort.join
+  end
+
+  def match(to_array)
+    to_array.select {|word| word.split("").sort.join == @sorted_word}
+  end
+
+end
