16a5e6e294bb0c92a582b37f7314b7780c8f8681
jaf7 <janthonyfields@gmail.com>
2018-02-03 21:22:50 -0500

    built anagram detector class (deleted comments)

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,3 @@
-# swap two consecutive indexes in the string, test, pass: return, fail: recurse
-# base case: string index == string length -1
-
-# In other words, given: "listen" and %w(enlists google inlets banana) the program should return ["inlets"]
-
- # It should return all matches in an array. If no matches exist, it should return an empty array.
-
-# listen = Anagram.new("listen")
-# listen.match(%w(enlists google inlets banana))
-# # => ["inlets"]
-
-require 'pry'
 class Anagram
   attr_accessor :word
 
@@ -19,7 +7,6 @@ class Anagram
 
   def match(arr)
     arr.select do |w|
-      # binding.pry
       w.split('').sort == @word.split('').sort
     end
   end
